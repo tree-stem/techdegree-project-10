95466c7f30c4f557c6de3735e0e3a4fe20a38946
Tristan Ringer <rin9er@gmail.com>
2025-03-27 16:54:36 -0700

    wrap App component in BrowserRouter tags

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/global.css';
-import './styles/reset.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App.jsx'
+
+import './styles/reset.css';
+import './styles/global.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
