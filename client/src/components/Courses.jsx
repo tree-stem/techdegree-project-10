e07391ec000411e66bd4db867deba8054b133146
Tristan Ringer <rin9er@gmail.com>
2025-03-28 16:34:45 -0700

    retrieve courses from api and display them in browser with navigation links

--- client/src/components/Courses.jsx
+++ client/src/components/Courses.jsx
@@ -1,28 +1,57 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 const Courses = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            try {
+                const response = await fetch("http://localhost:5000/api/courses");
+                if (response.status === 200) {
+                    const fetchedData = await response.json();
+                    setData(fetchedData);
+                    fetchedData.forEach(course => { console.log(course.title) });
+                    // console.log(JSON.stringify(data, null, 2));
+                } else if (response.status === 400) {
+                    console.log("Sorry! Page not found");
+                } else {
+                    throw new Error("Unexpected error occured");
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchCourses();
+    }, []);
+
     return (
         <main>
             <div className="wrap main--grid">
-                <a className="course--module course--link" href="course-detail.html">
-                    <h2 className="course--label">Course</h2>
-                    <h3 className="course--title">Build a Basic Bookcase</h3>
-                </a>
-                <a className="course--module course--link" href="course-detail.html">
-                    <h2 className="course--label">Course</h2>
-                    <h3 className="course--title">Learn How to Program</h3>
-                </a>
-                <a className="course--module course--link" href="course-detail.html">
-                    <h2 className="course--label">Course</h2>
-                    <h3 className="course--title">Learn How to Test Programs</h3>
-                </a>
-                <a className="course--module course--add--module" href="create-course.html">
-                    <span className="course--add--title">
-                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                            viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
-                        New Course
-                    </span>
-                </a>
+                {data ? (
+                    <>
+                        {data.map(course => (
+                            <Link key={course.id} className="course--module course--link" to={`courses/${course.id}`}>
+                                <h2 className="course--label">Course</h2>
+                                <h3 className="course--title">{course.title}</h3>
+                            </Link>
+                        ))}
+                        <Link className="course--module course--add--module" to="courses/create">
+                            <span className="course--add--title">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                                    viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                                </svg>
+                                New Course
+                            </span>
+                        </Link>
+                    </>
+                )
+                    : (
+                        <div>Loading...</div>
+                    )
+                }
             </div>
-        </main>
+        </main >
     )
 }
 
