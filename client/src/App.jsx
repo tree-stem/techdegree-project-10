15c2db1946ebe147ac2c0fae8edfa50af464d23b
Tristan Ringer <rin9er@gmail.com>
2025-03-30 21:02:36 -0700

    include routes for new components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header"
 import Courses from "./components/Courses";
 import CourseDetail from "./components/CourseDetail";
+import UserSignUp from "./components/UserSignUp";
+import UserSignIn from "./components/UserSignIn";
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
@@ -11,6 +14,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Courses />} />
         <Route path="/courses/:id" element={<CourseDetail />} />
+        <Route path="/signup" element={<UserSignUp />} />
+        <Route path="/signin" element={<UserSignIn />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   )
