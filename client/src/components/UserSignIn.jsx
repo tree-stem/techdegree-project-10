b24d69caf44b639ba9b6b918d159482ec9b3278d
Tristan Ringer <rin9er@gmail.com>
2025-03-31 12:10:56 -0700

    add event handler to fetch user credentials and sign user in

--- client/src/components/UserSignIn.jsx
+++ client/src/components/UserSignIn.jsx
@@ -1,17 +1,68 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
+
 const UserSignIn = () => {
+    const emailAddress = useRef(null);
+    const password = useRef(null);
+
+    // event handlers
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const credentials = {
+            emailAddress: emailAddress.current.value,
+            password: password.current.value
+        }
+
+        const encodedCredentials = btoa(`${credentials.emailAddress}:${credentials.password}`);
+
+        const fetchOptions = {
+            method: "GET",
+            headers: {
+                Authorization: `Basic ${encodedCredentials}`
+            }
+        }
+
+        const response = await fetch("http://localhost:5000/api/users", fetchOptions);
+        try {
+            if (response.status === 200) {
+                const user = await response.json();
+                console.log(user);
+                // console.log(`SUCCESS! ${user.} is now signed in!)
+            } else if (response.status === 401) {
+                const data = await response.json();
+                console.log(data);
+            } else {
+                throw new Error();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <main>
             <div className="form--centered">
                 <h2>Sign In</h2>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label htmlFor="emailAddress">Email Address</label>
-                    <input id="emailAddress" name="emailAddress" type="email" value="" />
+                    <input
+                        id="emailAddress"
+                        name="emailAddress"
+                        type="email"
+                        ref={emailAddress}
+                    />
                     <label htmlFor="password">Password</label>
-                    <input id="password" name="password" type="password" value="" />
-                    <button className="button" type="submit">Sign In</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        ref={password}
+                    />
+                    <button className="button" type="submit">Sign In</button>
+                    <button className="button button-secondary">Cancel</button>
                 </form>
                 <p>Don't have a user account? Click here to <Link to="signup">sign up!</Link></p>
 
