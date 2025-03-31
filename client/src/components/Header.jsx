47e2cecf84f7738c7687d82dd8f4a41851dc40ee
Tristan Ringer <rin9er@gmail.com>
2025-03-31 16:23:25 -0700

    made header dynamic by adding a condition with authUser

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,34 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import UserContext from '../context/UserContext.jsx';
+
 const Header = () => {
+    const { authUser } = useContext(UserContext);
+
     return (
         <header>
             <div className="wrap header--flex">
                 <h1 className="header--logo"><a href="index.html">Courses</a></h1>
                 <nav>
-                    <ul className="header--signedout">
-                        <li><Link to="/signup">Sign Up</Link></li>
-                        <li><Link to="/signin">Sign In</Link></li>
-                    </ul>
+                    {authUser === null ?
+                        <>
+                            <ul className="header--signedout">
+                                <li><Link to="/signup">Sign Up</Link></li>
+                                <li><Link to="/signin">Sign In</Link></li>
+                            </ul>
+                        </>
+                        :
+                        <>
+                            <ul className="header--signedout">
+                                <span>Welcome {authUser.firstName}</span>
+                                <li><Link to="/signout">Sign Out</Link></li>
+                            </ul>
+                        </>
+                    }
                 </nav>
             </div>
-        </header>
+        </header >
     )
 }
 
