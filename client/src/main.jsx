0b936aaf10e17a6e714fcc6d44d4e8aa0bafa4c1
Tristan Ringer <rin9er@gmail.com>
2025-03-31 15:47:27 -0700

    import UserProvider and wrap App component with it

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './context/UserContext.jsx';
 
 import App from './App.jsx'
 
@@ -10,7 +11,9 @@ import './styles/global.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>,
 )
