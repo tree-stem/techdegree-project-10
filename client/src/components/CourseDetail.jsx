2b076380cc6d5c80cf9ffd53ceea850522e916e3
Tristan Ringer <rin9er@gmail.com>
2025-04-01 17:04:30 -0700

    add conditional statements to make the course detail page dynamic

--- client/src/components/CourseDetail.jsx
+++ client/src/components/CourseDetail.jsx
@@ -1,9 +1,12 @@
 import { Link, useParams, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+
+import UserContext from '../context/UserContext';
 
 const CourseDetail = () => {
     const { id } = useParams();
     const [course, setCourse] = useState(null);
+    const { authUser } = useContext(UserContext);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -23,8 +26,8 @@ const CourseDetail = () => {
             }
         }
         fetchCourseData();
-    }, [id]);
 
+    }, [id]);
 
     const handleDelete = async (event) => {
         event.preventDefault();
@@ -51,55 +54,53 @@ const CourseDetail = () => {
 
     return (
         <main>
-            {course ? (
-                <>
-                    <div className="actions--bar">
-                        <div className="wrap">
-                            <Link className="button" to={`/courses/${id}/update`}>Update Course</Link>
-                            <Link className="button" to="/" onClick={handleDelete}>Delete Course</Link>
-                            <Link className="button button-secondary" to="/">Return to List</Link>
-                        </div>
-                    </div>
-
-                    <div className="wrap">
-                        <h2>Course Detail</h2>
-                        <form>
-                            <div className="main--flex">
-                                <div>
-                                    <h3 className="course--detail--title">Course</h3>
-                                    <h4 className="course--name">{course.title}</h4>
-                                    <p>{`By ${course.User.firstName} ${course.User.lastName}`}</p>
-
-                                    <p>{course.description}</p>
-                                </div>
-                                <div>
-                                    <h3 className="course--detail--title">Estimated Time</h3>
-                                    <p>{course.estimatedTime}</p>
-
-                                    <h3 className="course--detail--title">Materials Needed</h3>
-                                    {course.materialsNeeded && (
-                                        <ul className="course--detail--list">
-                                            {course.materialsNeeded
-                                                .split("\n")
-                                                .filter(material => material.trim() !== "")
-                                                .map((material, index) => (
-                                                    <li key={index}>{material.replace(/^\* /, "")}</li>
-                                                ))}
-                                        </ul>
-                                    )}
-                                </div>
+            {authUser ? (
+                course && course.userId === authUser.id ? (
+                    <>
+                        <div className="actions--bar">
+                            <div className="wrap">
+                                <Link className="button" to={`/courses/${id}/update`}>Update Course</Link>
+                                <Link className="button" to="/" onClick={handleDelete}>Delete Course</Link>
+                                <Link className="button button-secondary" to="/">Return to List</Link>
                             </div>
-                        </form>
-                    </div>
-                </>
-            )
-                :
-                (
-                    <div>
-                        Loading...
-                    </div>
-                )}
-        </main>
+                        </div>
+                    </>
+                ) : null
+            ) : null}
+
+            {course ? (
+                <div className="wrap">
+                    <h2>Course Detail</h2>
+                    <form>
+                        <div className="main--flex">
+                            <div>
+                                <h3 className="course--detail--title">Course</h3>
+                                <h4 className="course--name">{course.title}</h4>
+                                <p>{`By ${course.User.firstName} ${course.User.lastName}`}</p>
+
+                                <p>{course.description}</p>
+                            </div>
+                            <div>
+                                <h3 className="course--detail--title">Estimated Time</h3>
+                                <p>{course.estimatedTime}</p>
+
+                                <h3 className="course--detail--title">Materials Needed</h3>
+                                {course.materialsNeeded && (
+                                    <ul className="course--detail--list">
+                                        {course.materialsNeeded
+                                            .split("\n")
+                                            .filter(material => material.trim() !== "")
+                                            .map((material, index) => (
+                                                <li key={index}>{material.replace(/^\* /, "")}</li>
+                                            ))}
+                                    </ul>
+                                )}
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            ) : <div>Loading...</div>}
+        </main >
     )
 }
 
